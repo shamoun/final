ca5fcb48591b04c2f138a6af7b749b5ef93642d5
vagrant <vagrant@kellogg-dev-box>
2014-05-14 19:14:00 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+Classes.delete_all
+Classes.create("dept" => "HEMA", "class_no" => "914", "class_name" => "Biomedical Marketing")
+Classes.create("dept" => "MGMT", "class_no" => "441", "class_name" => "Intellectual Capital Management")
+Classes.create("dept" => "OPNS", "class_no" => "923", "class_name" => "Enterprise Risk Management")
+Classes.create("dept" => "KIEI", "class_no" => "925", "class_name" => "Startup Programming and Management")
+Classes.create("dept" => "FINC", "class_no" => "454", "class_name" => "Real Estate Finance and Investments")
+Classes.create("dept" => "HEMA", "class_no" => "453", "class_name" => "Managerial Challenges in the Pharmaceutical, Biotech, and Medical Device Industries")
+Classes.create("dept" => "MECN", "class_no" => "430", "class_name" => "Microeconomic Analysis")
+Classes.create("dept" => "INTL", "class_no" => "473", "class_name" => "Global Initiatives in Management")
+Classes.create("dept" => "ACCT", "class_no" => "430", "class_name" => "Accounting for Decision Making")
+Classes.create("dept" => "DECS", "class_no" => "431", "class_name" => "Business Analytics II")
+Classes.create("dept" => "FINC", "class_no" => "440", "class_name" => "Finance I/II")
+Classes.create("dept" => "MGMT", "class_no" => "431", "class_name" => "Business Strategy")
+Classes.create("dept" => "KIEI", "class_no" => "462", "class_name" => "New Venture Discovery")
+Classes.create("dept" => "ACCT", "class_no" => "434", "class_name" => "Turbo Accounting")
+Classes.create("dept" => "FINC", "class_no" => "940-A", "class_name" => "Financial Modeling for Leveraged Buyout Transactions")
+Classes.create("dept" => "ACCT", "class_no" => "452", "class_name" => "Financial Reporting and Analysis II")
 
 
 
