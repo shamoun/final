e93bed273f3484a1bcc6dbf329fedcc72c66127e
vagrant <vagrant@kellogg-dev-box>
2014-05-28 23:59:04 +0000

    signin stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def logout
+    cookies["user_id"] = nil
+    redirect_to "/", :notice => "See ya!"
+  end
+
+  def authenticate
+    the_user = User.find_by("username" => params["username"])
+    if the_user != nil
+      if the_user["password"] == params["password"]
+        cookies["user_id"] = the_user["id"]
+        redirect_to "/", :notice => "Welcome!"
+      else
+        redirect_to "/login", :notice => "Unknown password."
+      end
+    else
+      redirect_to "/login", :notice => "Unknown username."
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Final::Application.routes.draw do
   get "/" => "courses/index"
 
   get "/professors" => "professors/index"
-
   get "/professors/:id" => "professors/courses"
 
   get "/courses" => "courses/index"
-
   get "/courses/:id" => "courses/professors"
 
   get "/section/:id" => "section/show"
@@ -26,5 +24,28 @@ Final::Application.routes.draw do
 
   # --- Delete
   delete "/rating/:id" => 'rating#destroy'
+
+  # --- Create
+  get "/users/new" => 'users#new'
+  post "/users" => 'users#create'
+
+  # --- Read
+  get "/users" => 'users#index'
+  get "/users/:id" => 'users#show'
+
+  # -- Update
+  get "/users/:id/edit" => 'users#edit'
+  patch "/users/:id" => 'users#update'
+
+  # --- Delete
+  delete "/users/:id" => 'users#destroy'
+
+
+  # Sign-In and Sign-Out
+
+  get "/logout" => 'sessions#logout'
+  get "/login" => 'sessions#login'
+  post "/authenticate" => 'sessions#authenticate'
+
 
 end
