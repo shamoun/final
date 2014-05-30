5cd2288c9e5ac91e1010571a90a55e02407d0de4
vagrant <vagrant@kellogg-dev-box>
2014-05-30 18:29:07 +0000

    hi molly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527183444) do
+ActiveRecord::Schema.define(version: 20140530181916) do
 
   create_table "courses", force: true do |t|
     t.string  "dept"
