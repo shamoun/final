a18882eb371162ca68e4082781916d9109c327a4
vagrant <vagrant@kellogg-dev-box>
2014-06-02 01:39:40 +0000

    section data

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,14 +7,14 @@ class CoursesController < ApplicationController
   end
 
   def create
-    Course.create("dept" => params["dept"], "course_no" => params["course_no"], "coures_name" => params["course_name"])
+    Course.create("dept" => params["dept"], "course_no" => params["course_no"], "course_name" => params["course_name"])
     redirect_to "/"
   end
 
 
   def update
     course = Course.find_by("id" => params["id"])
-    course.update("dept" => params["dept"], "course_no" => params["course_no"], "coures_name" => params["course_name"])
+    course.update("dept" => params["dept"], "course_no" => params["course_no"], "course_name" => params["course_name"])
     redirect_to "/"
   end
 
