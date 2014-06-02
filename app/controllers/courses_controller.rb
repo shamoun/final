baca803041b0f0d2380961ce2d8fa21a523b4ce2
vagrant <vagrant@kellogg-dev-box>
2014-06-02 03:57:40 +0000

    hw: edit course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,21 +1,23 @@
 class CoursesController < ApplicationController
 
-  def destroy
+def create
+    Course.create("dept" => params["dept"],
+                 "course_no" => params["course_no"],
+                 "course_name" => params["course_name"])
+
+    redirect_to "/courses"
+  end
+
+def destroy
     course = Course.find_by("id" => params["id"])
     course.delete
-    redirect_to "/"
+    redirect_to "/courses"
   end
 
-  def create
-    Course.create("dept" => params["dept"], "course_no" => params["course_no"], "course_name" => params["course_name"])
-    redirect_to "/"
-  end
-
-
-  def update
+def update
     course = Course.find_by("id" => params["id"])
     course.update("dept" => params["dept"], "course_no" => params["course_no"], "course_name" => params["course_name"])
-    redirect_to "/"
+    redirect_to "/courses"
   end
 
 end
